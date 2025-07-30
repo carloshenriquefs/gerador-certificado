a8577d0302f090b678fc9c29088e544862f157bd
Carlos Henrique <ichfs9108@gmail.com>
2025-07-30 15:42:38 -0300

    componente: primary button

--- gerador-certificado/src/app/app.ts
+++ gerador-certificado/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
 import { CommonModule } from '@angular/common';
+import { PrimaryButton } from "./_components/primary-button/primary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
