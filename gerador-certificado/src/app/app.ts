c22fd96cfe8f4bcf28ce373c384bb5b519a546ce
Carlos Henrique <ichfs9108@gmail.com>
2025-07-30 17:04:22 -0300

    componente: item da lista de certificados

--- gerador-certificado/src/app/app.ts
+++ gerador-certificado/src/app/app.ts
@@ -4,10 +4,11 @@ import { Navbar } from './_components/navbar/navbar';
 import { CommonModule } from '@angular/common';
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
