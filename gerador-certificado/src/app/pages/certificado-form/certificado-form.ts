04640c0ebe61d3fd318ff25065261c3195d9336d
Carlos Henrique <ichfs9108@gmail.com>
2025-08-03 01:27:01 -0300

    validando os campos do formulario

--- gerador-certificado/src/app/pages/certificado-form/certificado-form.ts
+++ gerador-certificado/src/app/pages/certificado-form/certificado-form.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from '../../_components/primary-button/primary-button';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [PrimaryButton, SecondaryButton, FormsModule],
+  imports: [PrimaryButton, SecondaryButton, FormsModule, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css',
 })
@@ -13,4 +14,8 @@ export class CertificadoForm {
   nome: string = '';
   atividade: string = '';
   atividades: string[] = ['Angular', 'React', 'Java'];
+
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched;
+  }
 }
