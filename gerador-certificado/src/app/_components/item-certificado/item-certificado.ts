8f4ddebdc6592c0aa56c0bc068463e411bd6ec9a
Carlos Henrique <ichfs9108@gmail.com>
2025-08-04 16:42:41 -0300

    revisando a aplicacao antes do deploy

--- gerador-certificado/src/app/_components/item-certificado/item-certificado.ts
+++ gerador-certificado/src/app/_components/item-certificado/item-certificado.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { SecondaryButton } from '../secondary-button/secondary-button';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
--- gerador-certificado/src/app/_components/navbar/navbar.ts
+++ gerador-certificado/src/app/_components/navbar/navbar.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
@@ -8,12 +8,4 @@ import { RouterLink, RouterModule } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar implements OnInit {
-  ngOnInit(): void {
-    this.mensagem();
-  }
-
-  mensagem() {
-    console.log('Meu componente navbar inicializou (dentro de uma função)');
-  }
-}
+export class Navbar {}
